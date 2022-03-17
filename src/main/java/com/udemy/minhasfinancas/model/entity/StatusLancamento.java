f1bfcd2162aef5d1bdd79b6481da22ae988da94f
Doris Luvizute <dorisluvizute@gmail.com>
2022-03-17 10:05:05 -0300

    Criacao das entidades

---
+++ src/main/java/com/udemy/minhasfinancas/model/entity/StatusLancamento.java
@@ -0,0 +1,8 @@
+package com.udemy.minhasfinancas.model.entity;
+
+public enum StatusLancamento {
+
+	PENDENTE,
+	CANCELADO,
+	EFETIVADO
+}
---
+++ src/main/java/com/udemy/minhasfinancas/model/entity/TipoLancamento.java
@@ -0,0 +1,7 @@
+package com.udemy.minhasfinancas.model.entity;
+
+public enum TipoLancamento {
+
+	RECEITA,
+	DESPESA
+}
